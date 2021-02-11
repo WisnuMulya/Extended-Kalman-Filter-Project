8ee39489ccbfcbc9b3f557bc6f1ef83abc30e901
Wisnu Mulya <wisnu@wisnumulya.com>
2021-02-11 22:36:03 +0700

    Apply working scripts for sensor fusion & kalman filter

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -23,19 +23,65 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = (F_ * P_ * Ft) + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+  VectorXd y = z - (H_ * x_);
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S  = (H_ * P_ * Ht) + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  // measurement update
+  x_ = x_ + (K * y);
+  float x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+  // translate x state to radar measurement space
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+  
+  float rho = sqrt(px * px + py * py);
+  float theta = atan2(py, px);
+  float rho_dot = (px * vx + py * vy)/rho;
+
+  // calculate h_j(x)
+  VectorXd z_pred = VectorXd(3);
+  z_pred << rho,
+            theta,
+            rho_dot;
+  
+  //  VectorXd y = z - z_pred;
+  VectorXd y = z - z_pred;
+  
+  // normalize measurement: phi in y
+  double *phi = &y(1);
+  *phi = fmod(*phi, 2 * M_PI); // ensure -2pi < phi < 2pi
+  // ensure -pi < phi < pi
+  if (fabs(*phi) > M_PI) {
+    if (*phi > M_PI) {
+      *phi = *phi - (2 * M_PI);
+    } else {
+      *phi = *phi + (2 * M_PI);
+    }
+  }
+   
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S  = (H_ * P_ * Ht) + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  // measurement update
+  x_ = x_ + (K * y);
+  float x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,8 @@ int main() {
           ground_truth.push_back(gt_values);
           
           // Call ProcessMeasurement(meas_package) for Kalman filter
-          fusionEKF.ProcessMeasurement(meas_package);       
+          fusionEKF.ProcessMeasurement(meas_package);
+
 
           // Push the current estimated x,y positon from the Kalman filter's 
           //   state vector
